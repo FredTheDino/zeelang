6e1f8aab9bbc58b76434a1c20e66ac4a7555ff4f
Edvard Thörnros <edvard.thornros@gmail.com>
2020-04-10 09:58:14 +0200

    Clean the repo

--- compiler.py
+++ compiler.py
@@ -1,7 +1,11 @@
 import parser
+import sys
 
 if __name__ == "__main__":
     # Simple test for expression parsing.
     # parser.parse_file("simple.zee")
     # Simple test for expressions and one single variable.
-    parser.parse_file("simple1.zee")
+    if len(sys.argv) == 2:
+        parser.parse_file(sys.argv[1])
+    else:
+        print("Wrong number of arguments:", sys.argv)
