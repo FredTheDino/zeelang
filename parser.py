5e2ba599fb5bc118e56e5d7d3b3ba45a77abf19c
Edvard Thörnros <edvard.thornros@gmail.com>
2020-04-22 21:59:53 +0200

    Move lark file

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import code_gen
 
 def parse_file(filename, debug=False):
     # TODO(ed): This should be a function
-    with open("zee.bnf") as f:
+    with open("zee.lark") as f:
         parser = Lark(f.read(), parser="lalr", propagate_positions=True, start="program")
 
     with open(filename) as f:
